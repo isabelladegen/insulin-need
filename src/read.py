b4e4f181970e957d7576179b6fdcd74bb2549f10
idegen <degen.isabella@gmail.com>
2022-05-30 19:30:46 +0100

    started reading bg, cannot read folders with more entries files, need to find out which ones have no bg reading

--- src/read.py
+++ src/read.py
@@ -1,11 +1,29 @@
+import glob
 import zipfile
 from io import TextIOWrapper
 from pathlib import Path
 import logging
 import pandas as pd
+from src.configurations import Configuration
 
 
-def read_bg_values_from(file_name, config):
+# reads all BG files from each zip files without extracting the zip
+def read_all_bg(config: Configuration):
+    data = config.data_dir
+
+    # get all zip files in folder
+    filepaths = glob.glob(str(data) + "/*.zip")
+    dfs = []
+    for file in filepaths:
+        # Don't read aps zip just yet
+        if file.endswith(config.android_aps_zip):
+            continue
+        dfs.append(read_bg_from_zip(file, config))
+    return dfs
+
+
+# reads BGs into df from the entries csv file in the given zip file without extracting the zip
+def read_bg_from_zip(file_name, config):
     patient_id = Path(file_name).stem
 
     # find bg files in the zip file
@@ -13,7 +31,8 @@ def read_bg_values_from(file_name, config):
         files_and_folders = archive.namelist()
 
         # finds all the .csv files that contain BG readings in the zip
-        bg_entries_files = [x for x in files_and_folders if x.endswith(patient_id + config.bg_csv_file_extension)]
+        bg_entries_files = [x for x in files_and_folders if
+                            is_a_bg_csv_file(config, patient_id, x)]
 
         # log warning if there is more than one file
         if len(bg_entries_files) != 1:
@@ -21,6 +40,7 @@ def read_bg_values_from(file_name, config):
                 bg_entries_files))
 
         # read all these files into dataframes
+        df = None
         for file in bg_entries_files:
             info = archive.getinfo(file)
 
@@ -32,6 +52,17 @@ def read_bg_values_from(file_name, config):
             # read entries into pandas dataframe
             with archive.open(file, mode="r") as bg_file:
                 df = pd.read_csv(TextIOWrapper(bg_file, encoding="utf-8"), header=None, names=['time', 'bg'])
-            # add patient id to df
-            df['id'] = patient_id
-        return df
+                # add patient id to df
+        if df is None:
+            logging.warning("No entries file for zip file: " + file_name)
+        return df, patient_id
+
+
+# checks if a file from zip namelist is a bg csv file
+def is_a_bg_csv_file(config, patient_id, file_path):
+    # file starts with patient id and _entries
+    startswith = Path(file_path).name.startswith(patient_id + config.bg_csv_file_start)
+
+    # has right file ending
+    endswith = file_path.endswith(config.bg_csv_file_extension)
+    return startswith and endswith
--- tests/test_read.py
+++ tests/test_read.py
@@ -1,7 +1,9 @@
 import glob
+from pathlib import Path
+
 from hamcrest import *
 from src.configurations import TestConfiguration
-from src.read import read_bg_values_from
+from src.read import read_bg_from_zip, read_all_bg, is_a_bg_csv_file
 
 config = TestConfiguration()
 test_data_dir = config.data_dir
@@ -9,7 +11,18 @@ test_data_dir = config.data_dir
 filepaths = glob.glob(str(test_data_dir) + "/*.zip")
 
 
-def test_reads_zip_file():
+def test_reads_bg_from_given_zip_file():
     path = filepaths[1]
-    df = read_bg_values_from(path, config)
+    df, id = read_bg_from_zip(path, config)
     assert_that(df, is_not(empty()))
+    assert_that(Path(path).stem, is_(id))
+
+
+def test_reads_all_peoples_files():
+    dfs = read_all_bg(config)
+    assert_that(len(dfs), is_(145))
+
+
+def test_is_a_bg_csv_file():
+    example_name = 'direct-sharing-31/84984656_entries.json_csv/84984656_entries.json.csv'
+    assert_that(is_a_bg_csv_file(config, '84984656', example_name), is_(True))
